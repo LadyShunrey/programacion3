9a3e3d7e95eda7c19cfaa0c3499145e21cb439ea
Melisa Burlando <melisa.burlando@gmail.com>
2023-07-05 14:50:28 -0300

    terminado el ejercicio 2 del tp1

---
+++ TP_01/src/EJ_02/EJ_02.java
@@ -0,0 +1,17 @@
+package EJ_02;
+
+/*Considerando la implementación de la Lista realizado en el ejercicio anterior, comparar la
+complejidad computacional contra un array en las siguientes operaciones:
+	1.- Insertar al principio.
+	2.- Buscar un elemento en una posición.
+	3.- Determinar la cantidad de elementos.
+*/
+public class EJ_02 {
+	/*
+	 *  En un array o en un ArrayList? Por las dudas voy a considerar ambas opciones
+	 * 1) Insertar al principio tiene la misma complejidad para ambos O(1)
+	 * 2) Buscar elementos en los arrays es O(1) pero en la ListaVinculada es O(n) ya que tiene iterar por cada elemento
+	 * 3) En el Array es O(1) y en la ListaVinculada podemos poner el método size para que también sea O(1), llevamos una variable size que se incremente
+	 * cuando agregamos un elemento y decremente cuando borramos.
+	 * */
+}
