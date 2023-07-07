2430df28cbaf327261d483f7edd59b9a7957ee95
Melisa Burlando <melisa.burlando@gmail.com>
2023-07-07 12:21:54 -0300

    terminado el ejercicio 2 del tp2 (parte1)

--- TP_0201/src/EJ_02/EJ_02.java
+++ TP_0201/src/EJ_02/EJ_02.java
@@ -6,6 +6,10 @@ Implemente un algoritmo de ordenamiento quicksort para un arreglo de tamaño N.
 2. ¿Cuál es su complejidad promedio?*/
 
 public class EJ_02 {
+	private static int [] numbers;
+	private static int [] helper;
+	private static int size;
+	
 	public static void main(String[] args) {
 		int [] arreglo = new int [5];
 		
@@ -20,7 +24,7 @@ public class EJ_02 {
 			System.out.print("["+arreglo[i] + "] ");	
 		}
 		
-		ordenarPorMergeSort(arreglo);
+		sort(arreglo);
 		
 		System.out.println("\n\nEl arreglo1 ORDENADO queda: \n");
 		for(int i=0;i < arreglo.length;i++){
@@ -40,19 +44,124 @@ public class EJ_02 {
 			System.out.print("["+arreglo2[i] + "] ");	
 		}
 		
-		ordenarPorQuickSort(arreglo2);
+		sort(arreglo2);
 		
 		System.out.println("\n\nEl arreglo2 ORDENADO queda: \n");
 		for(int i=0;i < arreglo2.length;i++){
 			System.out.print("["+arreglo2[i]+"] ");	
 		}
+		
+		int [] arreglo3 = new int [20];
+		
+		arreglo3[0] = 12;
+		arreglo3[1] = 2;
+		arreglo3[2] = 35;
+		arreglo3[3] = 7;
+		arreglo3[4] = -10;
+		arreglo3[5] = -50;
+		arreglo3[6] = 100;
+		arreglo3[7] = 60;
+		arreglo3[8] = 111;
+		arreglo3[9] = 22;
+		arreglo3[10] = 23;
+		arreglo3[11] = 90;
+		arreglo3[12] = 10;
+		arreglo3[13] = 7;
+		arreglo3[14] = 7;
+		arreglo3[15] = 65;
+		arreglo3[16] = 56;
+		arreglo3[17] = 44;
+		arreglo3[18] = 33;
+		arreglo3[19] = 1;
+
+		System.out.println("\n----\nEl arreglo3 es: \n");
+		for(int i=0;i < arreglo3.length;i++){
+			System.out.print("["+arreglo3[i] + "] ");	
+		}
+		
+		ordenarPorQuickSort(arreglo3);
+		
+		System.out.println("\n\nEl arreglo3 ORDENADO queda: \n");
+		for(int i=0;i < arreglo3.length;i++){
+			System.out.print("["+arreglo3[i]+"] ");	
+		}
 	}
 
-	public static void ordenarPorMergeSort(int[]arreglo){
+	public static void sort(int[] values){
+		numbers = values;
+		size = values.length;
+		helper = new int [size];
+		ordenarPorMergeSort(0, size-1);
+	}
+	
+	public static void ordenarPorMergeSort(int low, int high){
+		if(low<high){
+			int middle = (low+high)/2;
+			ordenarPorMergeSort(low, middle);
+			ordenarPorMergeSort(middle+1, high);
+			merge(low, middle, high);
+		}
+	}
+	
+	public static void merge(int low, int middle, int high){
+		for(int i = low; i<=high; i++){
+			helper[i] = numbers[i];
+		}
+		int i = low;
+		int j = middle+1;
+		int k = low;
 		
+		while(i<=middle && j<=high){
+			if(helper[i]<=helper[j]){
+				numbers[k]= helper[i];
+				i++;
+			}
+			else{
+				numbers[k]=helper[j];
+				j++;
+			}
+			k++;
+		}
+		while(i<=middle){
+			numbers[k] = helper[i];
+			k++;
+			i++;
+		}
+		while(j<=high){
+			numbers[k]=helper[j];
+			k++;
+			j++;
+		}
 	}
 	
 	public static void ordenarPorQuickSort(int[]arreglo){
-		
+		quickSort(arreglo, 0, arreglo.length - 1);
+	}
+
+	private static void quickSort(int[] arreglo, int low, int high) {
+		if (low < high) {
+	        int pivotIndex = partition(arreglo, low, high);
+	        quickSort(arreglo, low, pivotIndex - 1);
+	        quickSort(arreglo, pivotIndex + 1, high);
+	    }
+	}
+
+	private static int partition(int[] arreglo, int low, int high) {
+		int pivot = arreglo[high];
+	    int i = low - 1;
+	    for (int j = low; j < high; j++) {
+	        if (arreglo[j] < pivot) {
+	            i++;
+	            swap(arreglo, i, j);
+	        }
+	    }
+	    swap(arreglo, i + 1, high);
+	    return i + 1;
+	}
+
+	private static void swap(int[] arreglo, int i, int j) {
+		int temp = arreglo[i];
+	    arreglo[i] = arreglo[j];
+	    arreglo[j] = temp;
 	}
 }
