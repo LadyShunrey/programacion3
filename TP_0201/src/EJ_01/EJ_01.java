f89aefa8188ddef17b59d3df641ce5cb46f329da
Melisa Burlando <melisa.burlando@gmail.com>
2023-07-06 23:50:52 -0300

    arranco el ejercicio 1 del tp2

--- TP_0201/src/EJ_01/EJ_01.java
+++ TP_0201/src/EJ_01/EJ_01.java
@@ -1,39 +1,73 @@
 package EJ_01;
 
+/*Implemente un algoritmo de ordenamiento por selección en un arreglo.
+Implemente un algoritmo de ordenamiento por burbujeo en un arreglo.
+1. ¿Qué complejidad O tienen estos algoritmos?
+*/
+
 public class EJ_01 {
-	public int max;
-	public int[] arreglo = new int[max];
-	
-	//ordenar por seleccion
-	public void ordenarPorSeleccion(int[] arregloParaOrdenar){
-		for(int i = 0; i < max - 1; i++){
-			int min = i;
-			for(int j = i + 1; j<max; j++){
-				if(arreglo[j]<arreglo[min]){
-					min = j;
+
+	public static void main(String[] args) {
+		int [] arreglo = new int [5];
+		
+		arreglo[0] = 1;
+		arreglo[1] = 12;
+		arreglo[2] = 5;
+		arreglo[3] = 77;
+		arreglo[4] = 10;
+
+		System.out.println("El arreglo1 es: \n");
+		for(int i=0;i < arreglo.length;i++){
+			System.out.print("["+arreglo[i] + "] ");	
+		}
+		
+		ordenarPorSeleccion(arreglo);
+		
+		System.out.println("\n\nEl arreglo1 ORDENADO queda: \n");
+		for(int i=0;i < arreglo.length;i++){
+			System.out.print("["+arreglo[i]+"] ");	
+		}
+		
+		int [] arreglo2 = new int [5];
+		
+		arreglo2[0] = 12;
+		arreglo2[1] = 2;
+		arreglo2[2] = 35;
+		arreglo2[3] = 7;
+		arreglo2[4] = -10;
+
+		System.out.println("\n----\nEl arreglo2 es: \n");
+		for(int i=0;i < arreglo2.length;i++){
+			System.out.print("["+arreglo2[i] + "] ");	
+		}
+		
+		ordenarPorBurbujeo(arreglo2);
+		
+		System.out.println("\n\nEl arreglo2 ORDENADO queda: \n");
+		for(int i=0;i < arreglo2.length;i++){
+			System.out.print("["+arreglo2[i]+"] ");	
+		}
+	}
+
+	public static void ordenarPorSeleccion(int[]arreglo){
+		for(int i=0;i<arreglo.length-1;i++){
+			int minIndex = i;
+			for(int j=i+1; j<arreglo.length;j++){
+				if(arreglo[j]<arreglo[minIndex]){
+					minIndex = j;
 				}
 			}
-			int aux = arreglo[min];
-			arreglo[min] = arreglo[i];
-			arreglo[i] = aux;
+			int aux = arreglo[i];
+			arreglo[i] = arreglo[minIndex];
+			arreglo[minIndex] = aux;
 		}
 	}
 	
-	//ordenar por burbujeo
-	public void ordenarPorBurbujeo(int[] arregloParaOrdenar){
-		int i;
-		int j;
-		int aux;
-		for (i=0;i<max-1;i++){
-			for(j=0;j<max-1-i;j++){
-				if(arreglo[j]>arreglo[j+1]){
-					aux = arreglo[j+1];
-					arreglo[j+1] = arreglo[j];
-					arreglo[j] = aux;
-				}
+	public static void ordenarPorBurbujeo(int[]arreglo){
+		for(int i=0; i<arreglo.length;i++){
+			for(int j=i+1;j<arreglo.length-i;j++){
+				
 			}
 		}
 	}
-	//Qué complejidad tienen estos algoritmos?
-	//rta: O(n^2)
 }
---
+++ TP_0201/src/Redoing/EJ_01.java
@@ -0,0 +1,39 @@
+package Redoing;
+
+public class EJ_01 {
+	public int max;
+	public int[] arreglo = new int[max];
+	
+	//ordenar por seleccion
+	public void ordenarPorSeleccion(int[] arregloParaOrdenar){
+		for(int i = 0; i < max - 1; i++){
+			int min = i;
+			for(int j = i + 1; j<max; j++){
+				if(arreglo[j]<arreglo[min]){
+					min = j;
+				}
+			}
+			int aux = arreglo[min];
+			arreglo[min] = arreglo[i];
+			arreglo[i] = aux;
+		}
+	}
+	
+	//ordenar por burbujeo
+	public void ordenarPorBurbujeo(int[] arregloParaOrdenar){
+		int i;
+		int j;
+		int aux;
+		for (i=0;i<max-1;i++){
+			for(j=0;j<max-1-i;j++){
+				if(arreglo[j]>arreglo[j+1]){
+					aux = arreglo[j+1];
+					arreglo[j+1] = arreglo[j];
+					arreglo[j] = aux;
+				}
+			}
+		}
+	}
+	//Qué complejidad tienen estos algoritmos?
+	//rta: O(n^2)
+}
