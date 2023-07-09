461a845c271c0427a4b95be6afd3d6bccc7e7260
Melisa Burlando <melisa.burlando@gmail.com>
2023-07-08 22:58:07 -0300

    Terminado el ejercicio 5 del tp2 (parte2)

--- TP_0202/src/EJ_05/EJ_05.java
+++ TP_0202/src/EJ_05/EJ_05.java
@@ -1,5 +1,5 @@
 package EJ_05;
-
+import java.util.ArrayList;
 /*Dado un árbol binario donde todos los nodos poseen un carácter, de manera que cada rama del
 árbol contiene una palabra, implementar un algoritmo que busque y retorne todas las palabras
 que posea exactamente N vocales (ni más ni menos). Por ejemplo, para el siguiente árbol, con
@@ -8,6 +8,7 @@ retornar [“MANA”, “MANO”, “MISA”].
 */
 
 public class EJ_05 {
+	private static ArrayList<ArrayList<Nodo>> solucion = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		
@@ -32,19 +33,85 @@ public class EJ_05 {
 		ArbolBinarioCaracter arbol = new ArbolBinarioCaracter(m);
 		
 		System.out.println(arbol);
+		ArrayList<Nodo> solucionParcial = new ArrayList<>();
 		
+		imprimirRama(m, solucionParcial);
+		System.out.println("Todas las ramas: "+solucion);
+		solucion.clear();
+		
+		int vocalesPermitidas = 1;
+		imprimirRamaConVocales(m, solucionParcial, vocalesPermitidas);
+		System.out.println("Todas las ramas con una sola vocal: "+solucion);
+		solucion.clear();
+		
+		vocalesPermitidas = 2;
+		imprimirRamaConVocales(m, solucionParcial, vocalesPermitidas);
+		System.out.println("Todas las ramas con dos vocales: "+solucion);
 	}
 	
-	//imprimir rama
-	//imprimirramaconvocal
-	
-	//recorrer una rama e imprimirla
-	
-	//recorrer una rama e imprimirla solo si la suma de a,e,i,o,u es igual a N un contador++
-	
-	//IDEA:
-	//un ArraList de ArrayList de Character/String donde voy guardando en un arraylist Char todos y cuando llego a una hoja agarro ese ArrayList
-	//y lo agrego al array de arrays
+	//imprimir cada rama entera
+	public static void imprimirRama(Nodo raiz, ArrayList<Nodo> solucionParcial){
+		solucionParcial.add(raiz);
+		if(raiz.getDerecho()==null && raiz.getIzquierdo()==null){
+			ArrayList<Nodo> aux = new ArrayList<>();
+			for(Nodo nodo: solucionParcial){
+				aux.add(nodo);
+			}
+			solucion.add(aux);
+		}
+		if(raiz.getIzquierdo()!=null){
+			imprimirRama(raiz.getIzquierdo(), solucionParcial);
+		}
 		
+		if(raiz.getDerecho()!=null){
+			imprimirRama(raiz.getDerecho(), solucionParcial);
+		}
+		solucionParcial.remove(raiz);	
+	}
 	
+	//imprimirramaconvocal
+	public static void imprimirRamaConVocales(Nodo raiz, ArrayList<Nodo> solucionParcial, int vocalesPermitidas){
+		solucionParcial.add(raiz);
+		if(raiz.getDerecho()==null && raiz.getIzquierdo()==null){
+			int contador = 0;
+			for(Nodo nodo: solucionParcial){
+				if (nodo.getValor()=='a'){
+					System.out.println("Contiene a");
+					contador++;
+				}
+				if (nodo.getValor()=='e'){
+					System.out.println("Contiene e");
+					contador++;
+				}
+				if (nodo.getValor()=='i'){
+					System.out.println("Contiene i");
+					contador++;
+				}
+				if (nodo.getValor()=='o'){
+					System.out.println("Contiene o");
+					contador++;
+				}
+				if (nodo.getValor()=='u'){
+					System.out.println("Contiene u");
+					contador++;
+				}
+			}
+			System.out.println("Esta palabra tiene vocales: " + contador);
+			if(contador==vocalesPermitidas){
+				ArrayList<Nodo> aux = new ArrayList<>();
+				for(Nodo nodo: solucionParcial){
+					aux.add(nodo);
+				}
+				solucion.add(aux);			
+			}
+		}
+		if(raiz.getIzquierdo()!=null){
+			imprimirRamaConVocales(raiz.getIzquierdo(), solucionParcial, vocalesPermitidas);
+		}
+		
+		if(raiz.getDerecho()!=null){
+			imprimirRamaConVocales(raiz.getDerecho(), solucionParcial, vocalesPermitidas);
+		}
+		solucionParcial.remove(raiz);	
+	}
 }
